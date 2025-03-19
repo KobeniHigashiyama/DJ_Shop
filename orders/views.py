d61faf5deb1098d7a3ea705a1271acb0009a8988
Shakzod Usmanov <shakhzodusmanov2@gmail.com>
2025-03-19 15:31:06 +0100

    Order and html

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Order,OrderItem
+from .forms import OrderForm
+from cart.cart import Cart
 
-# Create your views here.
+
+
+def order_create(request):
+    cart= Cart(request)
+    if request.method == 'POST':
+        form = OrderForm(request.POST, request=request)
+        if form.is_valid():
+            order = form.save(commit=False)
+            for item in cart:
+                OrderItem.objects.create(order=order, product =item['product'],price=item['price'],quantity=item['quantity'])
+
+            cart.clear()
+            return render(request,'order/create.html',
+                          {'form': form, 'order': order})
+        else:
+            form = OrderForm(request=request)
+            return render (request,
+                           'order/create.html',
+                           {'form': form, 'cart': cart})
